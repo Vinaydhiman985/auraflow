5371cd7645802248e16910913111c18480fa41f4
Vinay Dhiman <vinaydhiman289@gmail.com>
2026-05-28 17:39:01 +0530

    Create Api.js

---
+++ client/src/Api.js
@@ -0,0 +1,14 @@
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
+export const api = async (endpoint, options = {}) => {
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    },
+    ...options,
+  });
+  return res.json();
+};
+
+export default API_URL;
